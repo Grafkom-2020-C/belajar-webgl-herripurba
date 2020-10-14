11fc1e53674897f8e48a80b45b0c6ad53613ced6
herripurba <58382561+herripurba@users.noreply.github.com>
2020-10-14 22:54:21 +0700

    Add files via upload

---
+++ Berhasil menggambar titik/main.js
@@ -0,0 +1,48 @@
+function main() {
+    var canvas = document.getElementById('myCanvas');
+    var gl =canvas.getContext("webgl");
+
+    // ibaratkan di bawah ini adalah .c
+    var vertexShaderSource  = `
+        void main() {
+            gl_PointSize = 20.0;
+            gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+        }
+    `;
+    var fragmentShaderSource = `
+        void main() {
+            gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+        }
+    `;
+
+    // Ibaratkan di bawah ini adalah .o
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
+
+
+    // ibarat  mengetikkan source code ke dalam penampung .c 
+    gl.shaderSource(vertexShader, vertexShaderSource);
+    gl.shaderSource(fragmentShader, fragmentShaderSource);
+
+    // ibarat mengompilasi .c menjadi .o
+    gl.compileShader(vertexShader);
+    gl.compileShader(fragmentShader);
+
+    // ibarat membuatkan penampung .exe
+    var shaderProgram = gl.createProgram();
+
+    // ibarat memasukkan "adonan " .o ke dalam penampung .exe
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+
+    // ibarat menggabung-gabungkan "adonan" yang ada dalam penampung .exe
+    gl.linkProgram(shaderProgram);
+
+    // ibarat memmulai menggunakan "cat" .exe ke dalam konteks grafika(penggambaran)
+    gl.useProgram(shaderProgram); 
+
+    gl.clearColor(0.0, 0.22, 0.5, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
